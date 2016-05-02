3b0bda19209655dd1b7b073d8efd9d3fbe3460da
egorovma93 <egorovma93@gmail.com>
2016-05-03 02:12:18 +0300

    Задание №3

---
+++ Spring-and-ajax-master/SpringSecurityTest/src/main/java/org/develnotes/examples/LoginController.java
@@ -0,0 +1,35 @@
+package org.develnotes.examples;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class LoginController {
+	
+	private final String ERROR_MESSAGE = "Ќеверные логин или пароль";
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)  // начальна€ страница
+	public String home() {
+		return "login";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+
+	@RequestMapping(value = "/loginfailed", method = RequestMethod.GET)
+	public String loginerror(ModelMap model) {
+		model.addAttribute("error", ERROR_MESSAGE);
+		return "login";
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout() {
+		return "login";
+	}
+	
+}
